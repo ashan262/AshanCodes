156b1e37b42946be510a3aa4e815d757e69d957d
Risky khan <khan.ashan.khan1234@gmail.com>
2023-01-01 16:47:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,40 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:190, origin:'bottom'})
 
 sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+
+// function for contact form submision
+
+var form = document.getElementById("my-form");
+		
+		async function handleSubmit(event) {
+		  event.preventDefault();
+		  var status = document.getElementById("my-form-status");
+		  var data = new FormData(event.target);
+		  fetch(event.target.action, {
+			method: form.method,
+			body: data,
+			headers: {
+				'Accept': 'application/json'
+			}
+		  }).then(response => {
+			if (response.ok) {
+			  status.innerHTML = "Thanks for your submission!";
+			  form.reset()
+			} else {
+			  response.json().then(data => {
+				if (Object.hasOwn(data, 'errors')) {
+				  status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+				} else {
+				  status.innerHTML = "Oops! There was a problem submitting your form"
+				}
+			  })
+			}
+		  }).catch(error => {
+			status.innerHTML = "Oops! There was a problem submitting your form"
+		  });
+		}
+		form.addEventListener("submit", handleSubmit)
+
+
+
+
